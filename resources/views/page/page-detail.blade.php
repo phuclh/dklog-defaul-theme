0825a24ab8c384edae5b4586813b20488ae6f52a
Phuc Le <phuclh93@gmail.com>
2021-10-01 01:04:40 -0700

    Add page detail view

---
+++ resources/views/page/page-detail.blade.php
@@ -0,0 +1,34 @@
+@push('seo')
+    <x-dklog::meta-tags
+        :title="$page->metaTag->title"
+        :description="$page->metaTag->description"
+        :image="$page->metaTag->imageUrl"/>
+@endpush
+
+<div class="max-w-4xl mx-auto pt-4 pb-2 md:pt-8 font-mono">
+    <x-slot name="title">{{ $page->title }}</x-slot>
+
+    @include('dklog::partials.header')
+
+    <div class="container mx-auto px-5">
+        <div class="mb-14">
+            <h1 class="leading-normal block text-gray-800 text-4xl font-semibold dark:text-white">
+                {{ $page->title }}
+            </h1>
+
+            <p class="text-sm mt-2 uppercase dark:text-blue-200">
+                <span>{{ $page->updated_at->format('F d, Y') }}</span>
+
+                @auth
+                    | <span><a href="{{ route('admin.pages.edit', $page) }}" class="hover:underline">Edit</a></span>
+                @endif
+            </p>
+        </div>
+
+        <article class="prose dark:prose-light max-w-none leading-10">
+            <div>
+                {!! $page->htmlContent !!}
+            </div>
+        </article>
+    </div>
+</div>
